5b4f535355667c0c9c6aaa79019ec848ba1c8ddf
Herb Sutter <herb.sutter@gmail.com>
2022-10-01 15:09:38 -0700

    Added comment about alternative to `aligned_storage`, closes #20

--- include/cpp2util.h
+++ include/cpp2util.h
@@ -384,7 +384,7 @@ using in =
 template<typename T>
 class deferred_init {
     bool init = false;
-    std::aligned_storage<sizeof(T), alignof(T)> data;
+    std::aligned_storage<sizeof(T), alignof(T)> data; // or: alignas(T) std::byte data[sizeof(T)];
 
     auto t() -> T& { return *std::launder(reinterpret_cast<T*>(&data)); }
 
