ba785f92a06cb989cfa05b43c66227b01a0db361
Herb Sutter <herb.sutter@gmail.com>
2022-12-05 16:49:36 -0800

    Correct fix to #158

--- include/cpp2util.h
+++ include/cpp2util.h
@@ -131,7 +131,9 @@
         #include <iterator>
         #include <ranges>
         #include <algorithm>
-        #if defined(__GNUC__) && __GNUC__ < 11
+        // GCC sometime after version 10 got a dependency on linking TBB if <execution> is
+        // included, so let's not pull in that header in our "import std;" simulation mode
+        #if not defined(__GNUC__) || __GNUC__ < 11
             #include <execution>
         #endif
         #include <bit>
