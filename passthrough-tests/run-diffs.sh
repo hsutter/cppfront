817893dc35e4e1991e57921b6dc39b9946141274
Herb Sutter <herb.sutter@gmail.com>
2022-12-10 17:26:47 -0800

    Add a shell script for running the `passthrough-tests`

---
+++ passthrough-tests/run-diffs.sh
@@ -0,0 +1,8 @@
+/mnt/c/GitHub/cppfront/x64/Debug/cppfront.exe msvc-msstl-e.cpp2
+diff -s -q -w msvc-msstl-e.cpp2 msvc-msstl-e.cpp
+
+/mnt/c/GitHub/cppfront/x64/Debug/cppfront.exe gcc-10-libstdc++-e.cpp2
+diff -s -q -w gcc-10-libstdc++-e.cpp2 gcc-10-libstdc++-e.cpp
+
+/mnt/c/GitHub/cppfront/x64/Debug/cppfront.exe clang-12-libstdc++-e.cpp2
+diff -s -q -w clang-12-libstdc++-e.cpp2 clang-12-libstdc++-e.cpp
