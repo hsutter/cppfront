3f2219485b74defeb3e3cba7cb677ad6e4315995
Filip Sajdak <filip.sajdak@gmail.com>
2023-03-26 20:39:57 +0200

    Add regression tests

---
+++ regression-tests/test-results/pure2-various-string-literals.cpp
@@ -0,0 +1,23 @@
+
+#define CPP2_USE_MODULES         Yes
+
+#include "cpp2util.h"
+
+#line 1 "pure2-various-string-literals.cpp2"
+auto main() -> int;
+
+//=== Cpp2 definitions ==========================================================
+
+#line 1 "pure2-various-string-literals.cpp2"
+auto main() -> int{
+    auto s1 {u"u\""}; 
+    auto s2 {U"U\""}; 
+    auto s3 {u8"u8\""}; 
+    auto s4 {L"L\""}; 
+    auto s5 {R"(R")"}; 
+    auto s6 {uR"(uR")"}; 
+    auto s7 {u8R"(u8R")"}; 
+    auto s8 {UR"(UR")"}; 
+    auto s9 {LR"(LR")"}; 
+}
+
