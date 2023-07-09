52a27985c388344587f5220268b675eec25ec3d5
Herb Sutter <herb.sutter@gmail.com>
2023-07-09 10:22:12 -0700

    Update regression tests to change GCC version from 10 to 13
    Storing test results from a newer version than GCC 10 also paves the way to change the minimum supported GCC from 10 to 11, so we can take a dependency on the GCC 11 `requires`-in-declaration bugfix that wasn't backported to GCC 10
    
    This commit also catches up on test output that hadn't been captured for MSVC and Clang

---
+++ regression-tests/test-results/gcc-13/run-tests-gcc-13.sh
@@ -0,0 +1,25 @@
+# This is intended to be run in the /test-results/gcc-13 subdirectory
+# in a Linux shell with gcc 13 installed
+#
+cp ../*.cpp .
+rm -f *.output
+count=0
+exe_count=0
+gcc --version > gcc-version.output 2>&1
+for f in *.cpp
+do
+    printf "Starting gcc 13 %s\n" "$f"
+    g++ -I../../../include -std=c++20 -pthread -o test.exe $f > $f.output 2>&1
+    rm -f $f
+    let count=count+1
+    if test -f "test.exe"; then
+        let exe_count=exe_count+1
+        printf "    Starting executable\n"
+        ./test.exe > $f.execution 2>&1
+        rm -f test.exe
+    fi
+done
+rm -f *.obj *.exp *.lib
+printf "\nDone: %s .cpp tests compiled\n" "$count"
+printf "\n      %s .cpp executables generated and run\n" "$exe_count"
+echo "$(date +"%T")"
