16c8e3d902585f572cba53416e5ff057cec3d80a
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2023-07-17 20:38:17 -0400

    refactor: work around string conversion warning

--- include/cpp2util.h
+++ include/cpp2util.h
@@ -615,7 +615,7 @@ public:
                 *t = T(CPP2_FORWARD(args)...);
             }
             else {
-                Default.expects(!"attempted to copy assign, but copy assignment is not available");
+                Default.expects(false, "attempted to copy assign, but copy assignment is not available");
             }
         }
         else {
@@ -625,7 +625,7 @@ public:
                     dt->value() = T(CPP2_FORWARD(args)...);
                 }
                 else {
-                    Default.expects(!"attempted to copy assign, but copy assignment is not available");
+                    Default.expects(false, "attempted to copy assign, but copy assignment is not available");
                 }
             }
             else {
@@ -642,7 +642,7 @@ public:
                 *t = T{CPP2_FORWARD(args)...};
             }
             else {
-                Default.expects(!"attempted to copy assign, but copy assignment is not available");
+                Default.expects(false, "attempted to copy assign, but copy assignment is not available");
             }
         }
         else {
@@ -652,7 +652,7 @@ public:
                     dt->value() = T{CPP2_FORWARD(args)...};
                 }
                 else {
-                    Default.expects(!"attempted to copy assign, but copy assignment is not available");
+                    Default.expects(false, "attempted to copy assign, but copy assignment is not available");
                 }
             }
             else {
