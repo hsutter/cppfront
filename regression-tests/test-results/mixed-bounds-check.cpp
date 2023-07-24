e99aded88d6e09796bc7d2a13e075755435f0b74
jarzec <jarek.glowacki@gmail.com>
2023-07-24 17:44:40 +0200

    Fix tests and make them platform-indep (#556)

--- regression-tests/test-results/mixed-bounds-check.cpp
+++ regression-tests/test-results/mixed-bounds-check.cpp
@@ -21,6 +21,8 @@
 
 #line 4 "mixed-bounds-check.cpp2"
 [[nodiscard]] auto main() -> int{
+    std::set_terminate(std::abort);
+
     std::vector v {1, 2, 3, 4, 5, -999}; 
     CPP2_UFCS_0(pop_back, v);
     std::cout << cpp2::assert_in_bounds(std::move(v), 5) << "\n";
--- regression-tests/test-results/mixed-initialization-safety-3-contract-violation.cpp
+++ regression-tests/test-results/mixed-initialization-safety-3-contract-violation.cpp
@@ -17,14 +17,14 @@
 [[nodiscard]] auto main() -> int;
     
 
-#line 16 "mixed-initialization-safety-3-contract-violation.cpp2"
+#line 18 "mixed-initialization-safety-3-contract-violation.cpp2"
 auto fill(
     cpp2::out<std::string> x, 
     cpp2::in<std::string> value, 
     cpp2::in<int> count
     ) -> void;
 
-#line 26 "mixed-initialization-safety-3-contract-violation.cpp2"
+#line 28 "mixed-initialization-safety-3-contract-violation.cpp2"
 auto print_decorated(auto const& x) -> void;
 
 // for test determinism, force "fill" branch
@@ -40,6 +40,8 @@ bool flip_a_coin() {
 
 #line 5 "mixed-initialization-safety-3-contract-violation.cpp2"
 [[nodiscard]] auto main() -> int{
+    std::set_terminate(std::abort);
+
     cpp2::deferred_init<std::string> x; // note: uninitialized!
 
     if (flip_a_coin()) {
@@ -58,7 +60,7 @@ auto fill(
 
 {
     cpp2::Default.expects(cpp2::cmp_greater_eq(CPP2_UFCS_0(ssize, value),count), "fill: value must contain at least count elements");
-#line 23 "mixed-initialization-safety-3-contract-violation.cpp2"
+#line 25 "mixed-initialization-safety-3-contract-violation.cpp2"
     x.construct(CPP2_UFCS(substr, value, 0, count));
 }
 
