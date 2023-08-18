da782288664062d715f6cd4c90d94605f0706e2e
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2023-08-17 21:28:38 -0400

    test: add unit test for non local function expressions

---
+++ regression-tests/test-results/pure2-bugfix-for-non-local-function-expression.cpp
@@ -0,0 +1,33 @@
+
+#define CPP2_USE_MODULES         Yes
+
+//=== Cpp2 type declarations ====================================================
+
+
+#include "cpp2util.h"
+
+
+#line 5 "pure2-bugfix-for-non-local-function-expression.cpp2"
+class t;
+  
+
+//=== Cpp2 type definitions and function declarations ===========================
+
+#line 1 "pure2-bugfix-for-non-local-function-expression.cpp2"
+template<typename T> concept v = []() -> bool;
+(); 
+using u = std::type_identity_t<decltype([]() -> void;)>;
+
+class t: public std::type_identity_t<decltype([]() -> void;)> {
+
+};
+
+auto main() -> int;
+
+
+//=== Cpp2 function definitions =================================================
+
+
+#line 9 "pure2-bugfix-for-non-local-function-expression.cpp2"
+auto main() -> int{}
+
