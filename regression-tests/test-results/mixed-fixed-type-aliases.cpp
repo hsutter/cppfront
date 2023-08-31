ce8f8bcd9fe2960d6efd7f7b55905d8e97328d8e
jarzec <jarek.glowacki@gmail.com>
2023-08-31 21:09:39 +0200

    Improve/update tests (#590)

--- regression-tests/test-results/mixed-fixed-type-aliases.cpp
+++ regression-tests/test-results/mixed-fixed-type-aliases.cpp
@@ -9,25 +9,26 @@
 
 //=== Cpp2 type definitions and function declarations ===========================
 
-#include <typeinfo>
+#include <filesystem>
 #include <iostream>
+#include <typeinfo>
 
 namespace my {
     using u16 = float;
 }
 
-#line 8 "mixed-fixed-type-aliases.cpp2"
+#line 9 "mixed-fixed-type-aliases.cpp2"
 auto test(auto const& x) -> void;
     
 
-#line 15 "mixed-fixed-type-aliases.cpp2"
+#line 16 "mixed-fixed-type-aliases.cpp2"
 [[nodiscard]] auto main(int const argc_, char** argv_) -> int;
     
 
 //=== Cpp2 function definitions =================================================
 
 
-#line 8 "mixed-fixed-type-aliases.cpp2"
+#line 9 "mixed-fixed-type-aliases.cpp2"
 auto test(auto const& x) -> void{
     std::cout 
         << std::boolalpha 
@@ -37,7 +38,7 @@ auto test(auto const& x) -> void{
 
 [[nodiscard]] auto main(int const argc_, char** argv_) -> int{
     auto args = cpp2::make_args(argc_, argv_); 
-#line 16 "mixed-fixed-type-aliases.cpp2"
+#line 17 "mixed-fixed-type-aliases.cpp2"
     my::u16 y {42}; 
     test(std::move(y));
 
@@ -45,6 +46,6 @@ auto test(auto const& x) -> void{
     test(std::move(z));
 
     for ( auto const& arg : args ) 
-        std::cout << arg << "\n";
+        std::cout << CPP2_UFCS_0(filename, std::filesystem::path(arg)) << "\n";
 }
 
