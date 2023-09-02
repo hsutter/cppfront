4e374868177fa021a8124629cf8998bd74f3eeda
Herb Sutter <herb.sutter@gmail.com>
2023-09-01 17:57:49 -0700

    Update regression tests for latest PR merge, and add a Clang 12-vs-13+ compat note

--- regression-tests/test-results/pure2-bugfix-for-non-local-function-expression.cpp
+++ regression-tests/test-results/pure2-bugfix-for-non-local-function-expression.cpp
@@ -7,13 +7,17 @@
 #include "cpp2util.h"
 
 
-#line 5 "pure2-bugfix-for-non-local-function-expression.cpp2"
+#line 9 "pure2-bugfix-for-non-local-function-expression.cpp2"
 class t;
-  
+    
 
 //=== Cpp2 type definitions and function declarations ===========================
 
-#line 1 "pure2-bugfix-for-non-local-function-expression.cpp2"
+//  Clang note: When using Clang as the Cpp1 compiler, the following uses of an unnamed function
+//  require Clang 13 or higher; Clang 12 complains "lambda expression in an unevaluated operand"
+//  Standalone Cpp1 repro: https://godbolt.org/z/dznnYTvc6
+
+#line 5 "pure2-bugfix-for-non-local-function-expression.cpp2"
 template<typename T> concept v = []() -> bool { return true;  }(); 
 
 using u = std::type_identity_t<decltype([]() -> void{})>;
@@ -28,6 +32,6 @@ auto main() -> int;
 //=== Cpp2 function definitions =================================================
 
 
-#line 9 "pure2-bugfix-for-non-local-function-expression.cpp2"
+#line 13 "pure2-bugfix-for-non-local-function-expression.cpp2"
 auto main() -> int{}
 
