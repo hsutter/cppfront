d2e52f41921e2940d8e6bc524b07d9e9fc1c2918
Florian Knauf <knauf@vss.uni-hannover.de>
2023-12-13 11:32:42 +0100

    Added requested regression tests

--- regression-tests/test-results/pure2-for-loop-range-with-lambda.cpp
+++ regression-tests/test-results/pure2-for-loop-range-with-lambda.cpp
@@ -1,4 +1,5 @@
 
+#define CPP2_IMPORT_STD          Yes
 
 //=== Cpp2 type declarations ====================================================
 
@@ -11,14 +12,16 @@
 
 [[nodiscard]] auto first(auto&& f, [[maybe_unused]] auto&& ...param2) -> auto&&;
 
-auto main() -> int;
+auto main(int const argc_, char** argv_) -> int;
    
 
 //=== Cpp2 function definitions =================================================
 
 [[nodiscard]] auto first(auto&& f, [[maybe_unused]] auto&& ...param2) -> auto&& { return CPP2_FORWARD(f);  }
 
-auto main() -> int{
+auto main(int const argc_, char** argv_) -> int{
+   auto const args = cpp2::make_args(argc_, argv_); 
+#line 4 "pure2-for-loop-range-with-lambda.cpp2"
    std::array const ints {1, 2, 3, 4, 5}; 
    // OK
    for ( auto const& i : CPP2_UFCS_0(first, ints) ) {
@@ -40,5 +43,10 @@ auto main() -> int{
    for ( auto const& i : temp ) {
       std::cout << i;
    }
+
+   for ( auto const& i : [_0 = args]() -> auto { return _0;  }() ) static_cast<void>(i); 
+   for ( auto const& j : [](auto const& x) -> auto { return x;  }(args) ) static_cast<void>(j); 
+   for ( auto const& k : [](auto const& x) -> auto { return x;  }(args) )  { do static_cast<void>(k);  while (false); static_cast<void>([_0 = args]() -> auto { return _0;  }()); }
+   for ( auto const& l : [](auto const& x) -> auto { return x;  }(args) )  { do static_cast<void>(l);  while (false); static_cast<void>([](auto const& x) -> auto { return x;  }(args)); }
 }
 
