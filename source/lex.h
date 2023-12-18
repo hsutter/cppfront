dc7e10cdcc7ad6129414ca0e598dfb9f7f554827
Herb Sutter <herb.sutter@gmail.com>
2023-12-18 06:39:04 -1000

    Leave \ escapes in char literals in string interpolations
    Closes #861
    
    But " must still be escaped as \" inside a string interpolation expression. Removing that one need to escape inside a string interpolation would likely require switching to prefix $ -- see the #861 comment thread

--- source/lex.h
+++ source/lex.h
@@ -427,11 +427,16 @@ auto expand_string_literal(
 
             //  Then put interpolated chunk into ret
             auto chunk = std::string{text.substr(open, pos - open)};
-            { // unescape chunk string
-                auto last_it = std::remove_if(std::begin(chunk), std::end(chunk), [escape = false](const auto& e) mutable {
-                    escape = !escape && e == '\\';
-                    return escape;
-                });
+            {   // unescape chunk string
+                auto last_it = std::remove_if(
+                    std::begin(chunk),
+                    std::end(chunk),
+                    [escape = false, prev = ' '](const auto& e) mutable {
+                        escape = !escape && prev != '\'' && e == '\\';
+                        prev = e;
+                        return escape;
+                    }
+                );
                 chunk.erase(last_it, std::end(chunk));
             }
 
