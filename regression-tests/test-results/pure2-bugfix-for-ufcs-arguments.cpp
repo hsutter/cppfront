ec19f91847686466ae3aab1ad572817a92cf369a
jarzec <jarek.glowacki@gmail.com>
2024-01-17 04:14:15 +0100

    Update gcc and clang tests (#941)

--- regression-tests/test-results/pure2-bugfix-for-ufcs-arguments.cpp
+++ regression-tests/test-results/pure2-bugfix-for-ufcs-arguments.cpp
@@ -35,38 +35,38 @@ class B;
 class t {
   public: [[nodiscard]] auto f() & -> cpp2::i32;
   public: [[nodiscard]] auto f([[maybe_unused]] auto const& unnamed_param_2) & -> cpp2::i32;
-  public: template<typename UnnamedTypeParam1_10_7> [[nodiscard]] auto f() & -> cpp2::i32;
-  public: template<typename UnnamedTypeParam1_11_7> [[nodiscard]] auto f([[maybe_unused]] auto const& unnamed_param_2) & -> cpp2::i32;
-  public: template<typename UnnamedTypeParam1_12_7, typename U> [[nodiscard]] auto f([[maybe_unused]] auto const& unnamed_param_2, [[maybe_unused]] auto const& unnamed_param_3) & -> cpp2::i32;
+  public: template<typename UnnamedTypeParam1_1> [[nodiscard]] auto f() & -> cpp2::i32;
+  public: template<typename UnnamedTypeParam1_2> [[nodiscard]] auto f([[maybe_unused]] auto const& unnamed_param_2) & -> cpp2::i32;
+  public: template<typename UnnamedTypeParam1_3, typename U> [[nodiscard]] auto f([[maybe_unused]] auto const& unnamed_param_2, [[maybe_unused]] auto const& unnamed_param_3) & -> cpp2::i32;
 };
 
 [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1) -> cpp2::i32;
 [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1, [[maybe_unused]] auto const& unnamed_param_2) -> cpp2::i32;
-template<typename UnnamedTypeParam1_17_5> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1) -> cpp2::i32;
-template<typename UnnamedTypeParam1_18_5> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1, [[maybe_unused]] auto const& unnamed_param_2) -> cpp2::i32;
-template<typename UnnamedTypeParam1_19_5, typename U> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1, [[maybe_unused]] auto const& unnamed_param_2, [[maybe_unused]] auto const& unnamed_param_3) -> cpp2::i32;
+template<typename UnnamedTypeParam1_4> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1) -> cpp2::i32;
+template<typename UnnamedTypeParam1_5> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1, [[maybe_unused]] auto const& unnamed_param_2) -> cpp2::i32;
+template<typename UnnamedTypeParam1_6, typename U> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1, [[maybe_unused]] auto const& unnamed_param_2, [[maybe_unused]] auto const& unnamed_param_3) -> cpp2::i32;
 
 extern t m;
 extern t const n;
-template<typename UnnamedTypeParam1_23_5, typename U> auto inline constexpr a = n;
+template<typename UnnamedTypeParam1_7, typename U> auto inline constexpr a = n;
 
-extern cpp2::i32 auto_1;
-extern cpp2::i32 auto_2;
-extern cpp2::i32 auto_3;
-extern cpp2::i32 auto_4;
-extern cpp2::i32 auto_5;
-extern cpp2::i32 auto_6;
-extern cpp2::i32 auto_7;
 extern cpp2::i32 auto_8;
 extern cpp2::i32 auto_9;
 extern cpp2::i32 auto_10;
 extern cpp2::i32 auto_11;
+extern cpp2::i32 auto_12;
+extern cpp2::i32 auto_13;
+extern cpp2::i32 auto_14;
+extern cpp2::i32 auto_15;
+extern cpp2::i32 auto_16;
+extern cpp2::i32 auto_17;
+extern cpp2::i32 auto_18;
 
 auto main() -> int;
 
 #line 55 "pure2-bugfix-for-ufcs-arguments.cpp2"
 // _: i32 = 0.std::min<int>(0);
-extern cpp2::i32 auto_12;
+extern cpp2::i32 auto_19;
 
 namespace ns {
 template<int T, int U> class t {
@@ -98,33 +98,39 @@ class B {
   [[nodiscard]] auto t::f() & -> cpp2::i32 { return print_res(0);  }
 #line 9 "pure2-bugfix-for-ufcs-arguments.cpp2"
   [[nodiscard]] auto t::f([[maybe_unused]] auto const& unnamed_param_2) & -> cpp2::i32 { return print_res(1);  }
-  template<typename UnnamedTypeParam1_10_7> [[nodiscard]] auto t::f() & -> cpp2::i32 { return print_res(2);  }
-  template<typename UnnamedTypeParam1_11_7> [[nodiscard]] auto t::f([[maybe_unused]] auto const& unnamed_param_2) & -> cpp2::i32 { return print_res(3);  }
-  template<typename UnnamedTypeParam1_12_7, typename U> [[nodiscard]] auto t::f([[maybe_unused]] auto const& unnamed_param_2, [[maybe_unused]] auto const& unnamed_param_3) & -> cpp2::i32 { return print_res(4);  }
+#line 10 "pure2-bugfix-for-ufcs-arguments.cpp2"
+  template<typename UnnamedTypeParam1_1> [[nodiscard]] auto t::f() & -> cpp2::i32 { return print_res(2);  }
+#line 11 "pure2-bugfix-for-ufcs-arguments.cpp2"
+  template<typename UnnamedTypeParam1_2> [[nodiscard]] auto t::f([[maybe_unused]] auto const& unnamed_param_2) & -> cpp2::i32 { return print_res(3);  }
+#line 12 "pure2-bugfix-for-ufcs-arguments.cpp2"
+  template<typename UnnamedTypeParam1_3, typename U> [[nodiscard]] auto t::f([[maybe_unused]] auto const& unnamed_param_2, [[maybe_unused]] auto const& unnamed_param_3) & -> cpp2::i32 { return print_res(4);  }
 
 #line 15 "pure2-bugfix-for-ufcs-arguments.cpp2"
 [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1) -> cpp2::i32 { return print_res(5);  }
 #line 16 "pure2-bugfix-for-ufcs-arguments.cpp2"
 [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1, [[maybe_unused]] auto const& unnamed_param_2) -> cpp2::i32 { return print_res(6);  }
-template<typename UnnamedTypeParam1_17_5> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1) -> cpp2::i32 { return print_res(7);  }
-template<typename UnnamedTypeParam1_18_5> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1, [[maybe_unused]] auto const& unnamed_param_2) -> cpp2::i32 { return print_res(8);  }
-template<typename UnnamedTypeParam1_19_5, typename U> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1, [[maybe_unused]] auto const& unnamed_param_2, [[maybe_unused]] auto const& unnamed_param_3) -> cpp2::i32 { return print_res(9);  }
+#line 17 "pure2-bugfix-for-ufcs-arguments.cpp2"
+template<typename UnnamedTypeParam1_4> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1) -> cpp2::i32 { return print_res(7);  }
+#line 18 "pure2-bugfix-for-ufcs-arguments.cpp2"
+template<typename UnnamedTypeParam1_5> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1, [[maybe_unused]] auto const& unnamed_param_2) -> cpp2::i32 { return print_res(8);  }
+#line 19 "pure2-bugfix-for-ufcs-arguments.cpp2"
+template<typename UnnamedTypeParam1_6, typename U> [[nodiscard]] auto f([[maybe_unused]] cpp2::in<t> unnamed_param_1, [[maybe_unused]] auto const& unnamed_param_2, [[maybe_unused]] auto const& unnamed_param_3) -> cpp2::i32 { return print_res(9);  }
 
 t m {}; 
 t const n {}; 
 
 #line 25 "pure2-bugfix-for-ufcs-arguments.cpp2"
-cpp2::i32 auto_1 {CPP2_UFCS_NONLOCAL(f)(m)}; 
-cpp2::i32 auto_2 {CPP2_UFCS_NONLOCAL(f)(m, 0)}; 
-cpp2::i32 auto_3 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t>)(m)}; 
-cpp2::i32 auto_4 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t>)(m, 0)}; 
-cpp2::i32 auto_5 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t,t>)(m, 0, 0)}; 
-cpp2::i32 auto_6 {CPP2_UFCS_NONLOCAL(f)(n)}; 
-cpp2::i32 auto_7 {CPP2_UFCS_NONLOCAL(f)(n, 0)}; 
-cpp2::i32 auto_8 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t>)(n)}; 
-cpp2::i32 auto_9 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t>)(n, 0)}; 
-cpp2::i32 auto_10 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t,t>)(n, 0, 0)}; 
-cpp2::i32 auto_11 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t,t>)(a<t,t>, 0, 0)}; 
+cpp2::i32 auto_8 {CPP2_UFCS_NONLOCAL(f)(m)}; 
+cpp2::i32 auto_9 {CPP2_UFCS_NONLOCAL(f)(m, 0)}; 
+cpp2::i32 auto_10 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t>)(m)}; 
+cpp2::i32 auto_11 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t>)(m, 0)}; 
+cpp2::i32 auto_12 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t,t>)(m, 0, 0)}; 
+cpp2::i32 auto_13 {CPP2_UFCS_NONLOCAL(f)(n)}; 
+cpp2::i32 auto_14 {CPP2_UFCS_NONLOCAL(f)(n, 0)}; 
+cpp2::i32 auto_15 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t>)(n)}; 
+cpp2::i32 auto_16 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t>)(n, 0)}; 
+cpp2::i32 auto_17 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t,t>)(n, 0, 0)}; 
+cpp2::i32 auto_18 {CPP2_UFCS_TEMPLATE_NONLOCAL(f<t,t>)(a<t,t>, 0, 0)}; 
 
 #line 37 "pure2-bugfix-for-ufcs-arguments.cpp2"
 auto main() -> int{
@@ -146,7 +152,7 @@ auto main() -> int{
 }
 
 #line 56 "pure2-bugfix-for-ufcs-arguments.cpp2"
-cpp2::i32 auto_12 {CPP2_UFCS_QUALIFIED_TEMPLATE_NONLOCAL((ns::t<0,0>::),f<0>)(0)}; 
+cpp2::i32 auto_19 {CPP2_UFCS_QUALIFIED_TEMPLATE_NONLOCAL((ns::t<0,0>::),f<0>)(0)}; 
 
 namespace ns {
 
