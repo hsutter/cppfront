c14d78b2a1acaee17d7af20178c3c444922fe076
Neil Henderson <2060747+bluetarpmedia@users.noreply.github.com>
2024-01-18 11:54:37 +1000

    Strip any leading directory path from the argv[0] program name in `pure2-main-args` test to have consistent results on CI agent as well as local runs (#947)

--- regression-tests/test-results/pure2-main-args.cpp
+++ regression-tests/test-results/pure2-main-args.cpp
@@ -17,10 +17,18 @@ auto main(int const argc_, char** argv_) -> int;
 //=== Cpp2 function definitions =================================================
 
 #line 1 "pure2-main-args.cpp2"
-auto main(int const argc_, char** argv_) -> int { 
+auto main(int const argc_, char** argv_) -> int{
     auto const args = cpp2::make_args(argc_, argv_); 
 #line 2 "pure2-main-args.cpp2"
+    std::string exe {CPP2_ASSERT_IN_BOUNDS_LITERAL(args.argv, 0)}; 
+    auto pos {CPP2_UFCS(find_last_of)(exe, "/\\")}; 
+    if (pos != std::string::npos) {
+        ++pos;
+        exe = CPP2_UFCS(substr)(exe, pos, CPP2_UFCS(size)(exe) - std::move(pos));
+    }
     std::cout 
         << ("args.argc            is " + cpp2::to_string(args.argc) + "\n") 
-        << ("args.argv[0]         is " + cpp2::to_string(CPP2_ASSERT_IN_BOUNDS_LITERAL(args.argv, 0)) + "\n"); }
+        << ("args.argv[0]         is " + cpp2::to_string(std::move(exe)) + "\n");
+
+}
 
