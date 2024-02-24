d4bbc157af2e72767c87fa522fea128fe9bf00f8
Ingo Prötel <git@iprtel.de>
2024-02-24 16:24:30 +0100

    Fixed more expected files.

--- regression-tests/test-results/pure2-intro-example-three-loops.cpp
+++ regression-tests/test-results/pure2-intro-example-three-loops.cpp
@@ -21,7 +21,7 @@ auto decorate_and_print(auto& thing) -> void;
 
 #line 10 "pure2-intro-example-three-loops.cpp2"
 [[nodiscard]] auto main() -> int;
-#line 30 "pure2-intro-example-three-loops.cpp2"
+#line 35 "pure2-intro-example-three-loops.cpp2"
 
 #line 1 "pure2-intro-example-three-loops.cpp2"
 
@@ -45,7 +45,12 @@ auto decorate_and_print(auto& thing) -> void{
         "hello", "big", "world"}; 
     std::span<std::string> view {words}; 
 
-    auto i {cpp2_new<int>(0)}; 
+    auto p_i {cpp2_new<int>(0)}; 
+    if ((!(p_i))) {
+        return 1; 
+    }
+    auto i {*cpp2::assert_not_null(std::move(p_i))}; 
+
     for( ; cpp2::cmp_less(*cpp2::assert_not_null(i),CPP2_UFCS(ssize)(view)); ++*cpp2::assert_not_null(i) ) {
         print(CPP2_ASSERT_IN_BOUNDS(view, *cpp2::assert_not_null(i)));
     }
