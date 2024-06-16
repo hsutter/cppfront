a3e70a1802b80e9b4fa71eedec26e279b086638d
Herb Sutter <herb.sutter@gmail.com>
2024-06-16 15:28:15 -0700

    Add output to test to show that defaults were applied

--- regression-tests/test-results/mixed-default-arguments.cpp
+++ regression-tests/test-results/mixed-default-arguments.cpp
@@ -11,21 +11,26 @@
 //=== Cpp2 type definitions and function declarations ===========================
 
 #line 1 "mixed-default-arguments.cpp2"
-auto cxx(int x, std::string y) -> void {}
+auto cxx(int x, std::string y) -> void {
+    std::cout << x << " \"" << y << "\"" << "\n";
+}
 
-#line 3 "mixed-default-arguments.cpp2"
+#line 5 "mixed-default-arguments.cpp2"
 auto cxx2(cpp2::impl::in<int> x, cpp2::impl::in<std::string> y) -> void;
 
+#line 9 "mixed-default-arguments.cpp2"
 auto main() -> int;
 
 //=== Cpp2 function definitions =================================================
 
 #line 1 "mixed-default-arguments.cpp2"
 
-#line 3 "mixed-default-arguments.cpp2"
-auto cxx2(cpp2::impl::in<int> x, cpp2::impl::in<std::string> y) -> void{}
-
 #line 5 "mixed-default-arguments.cpp2"
+auto cxx2(cpp2::impl::in<int> x, cpp2::impl::in<std::string> y) -> void{
+    std::cout << (cpp2::to_string(x) + " \"" + cpp2::to_string(y) + "\"\n");
+}
+
+#line 9 "mixed-default-arguments.cpp2"
 auto main() -> int{
  cxx(1, "test");
  cxx({}, {});
